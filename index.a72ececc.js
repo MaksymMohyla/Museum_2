5cf67b46f6818f2d53470222f53efa4c18bc5e7c
Maksym Mohyla <greenparksquad@gmail.com>
2024-07-25 11:32:28 +0300

    make build

---
+++ index.a72ececc.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".dropdown");e.querySelector(".dropdown__button").addEventListener("click",()=>{e.classList.toggle("dropdown--active")});
+//# sourceMappingURL=index.a72ececc.js.map
